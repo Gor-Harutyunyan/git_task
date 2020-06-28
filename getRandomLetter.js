cfa86b85ac1de40bbdb73d088b6e476fbe54fd9f
Gor <Gor633369@mail.ru>
2020-06-28 14:28:11 +0400

    returned each files contnet

--- getRandomLetter.js
+++ getRandomLetter.js
@@ -1,7 +1,13 @@
-function getRandomNumber() {
-    return Math.random()
-}
+function getRandomLetter(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
 
-getRandomNumber();
-getRandomNumber();
-getRandomNumber();
+ getRandomLetter(3);
+ getRandomLetter(5);
+ getRandomLetter(10);
--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,13 +1,7 @@
-function getRandomLetter(length) {
-    let result = '';
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    let charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
- }
+function getRandomNumber() {
+    return Math.random()
+}
 
- getRandomLetter(3);
- getRandomLetter(5);
- getRandomLetter(10);
+getRandomNumber();
+getRandomNumber();
+getRandomNumber();
