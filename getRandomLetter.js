07941d1aac86d6bd123ee6917e69e87334468e7e
Gor <Gor633369@mail.ru>
2020-06-28 14:23:44 +0400

    swapped the contents

--- getRandomLetter.js
+++ getRandomLetter.js
@@ -1,9 +1,3 @@
-function getRandomLetter(length) {
-    let result = '';
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    let charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
- }
+function getRandomNumber() {
+    return Math.random()
+}
--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,3 +1,9 @@
-function getRandomNumber() {
-    return Math.random()
-}
+function getRandomLetter(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
